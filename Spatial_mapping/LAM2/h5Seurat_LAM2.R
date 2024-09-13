6499f72775f2b2b4f9a6047395185841d553297e
Lalit Kumar Gautam <84534730+gautam-lk@users.noreply.github.com>
2024-09-13 12:46:52 -0500

    Update h5Seurat_LAM2.R

--- Spatial_mapping/LAM2/h5Seurat_LAM2.R
+++ Spatial_mapping/LAM2/h5Seurat_LAM2.R
@@ -5,12 +5,8 @@ library(SeuratData)
 library(SeuratDisk)
 library(SeuratObject)
 
-
-
 ##Load Object
 LAM2_Spatial <- readRDS("spatial_data.RDS")
-
-
 
 SaveH5Seurat(LAM2_Spatial, filename = "LAM2_Spatial.h5Seurat")
 Convert("LAM2_Spatial.h5Seurat", dest = "h5ad") 
